db78608f81987bdf2272d6a26ed5f5eb682d07fd
almoussam607 <91743835+almoussam607@users.noreply.github.com>
2022-01-31 20:45:28 -0500

    Add files via upload

---
+++ Assignement#4.py
@@ -0,0 +1,18 @@
+x = input('Please enter yes to start the operation: ')
+
+while x == 'yes':
+    y = int(input('please enter a number to add by 1: '))
+    y += 1
+ 
+    print('The sum is', y)
+    x = input('Enter yes to continue: ')
+    
+    
+for m in range(0,1010,10):
+    print(m)
+
+     
+        
+
+     
+    
